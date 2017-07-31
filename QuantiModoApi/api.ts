62566a1667223728838f3046b4baf1650568b513
Mike P. Sinn <m@quantimodo.com>
2017-07-31 17:53:12 -0500

    5.8.731

---
+++ QuantiModoApi/api.ts
@@ -0,0 +1,17 @@
+export * from './AnalyticsApi';
+import { AnalyticsApi } from './AnalyticsApi';
+export * from './AuthenticationApi';
+import { AuthenticationApi } from './AuthenticationApi';
+export * from './ConnectorsApi';
+import { ConnectorsApi } from './ConnectorsApi';
+export * from './MeasurementsApi';
+import { MeasurementsApi } from './MeasurementsApi';
+export * from './RemindersApi';
+import { RemindersApi } from './RemindersApi';
+export * from './UnitsApi';
+import { UnitsApi } from './UnitsApi';
+export * from './UserApi';
+import { UserApi } from './UserApi';
+export * from './VariablesApi';
+import { VariablesApi } from './VariablesApi';
+export const APIS = [AnalyticsApi, AuthenticationApi, ConnectorsApi, MeasurementsApi, RemindersApi, UnitsApi, UserApi, VariablesApi];
